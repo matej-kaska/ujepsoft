5b26eba4256fefb4e74a8e57c2c4ce1f17ef824b
Matěj Kaška <nkaskaj@gmail.com>
2023-12-07 19:38:37 +0100

    Fixed styling

--- frontend/src/contexts/SnackbarProvider/ErrorSnackbar.tsx
+++ frontend/src/contexts/SnackbarProvider/ErrorSnackbar.tsx
@@ -8,7 +8,7 @@ const ErrorSnackbar = ({ message, fade, closeSnackbar }: ErrorSnackbarProps) => 
   <div className={`z-50 fixed top-0 left-1/2 transform -translate-x-1/2 mt-8 max-w-md whitespace-nowrap ${fade}`} onClick={closeSnackbar}>
     <div className="bg-red-100 border border-red-800 text-red-800 rounded relativ px-4 py-3 shadow-md">
       <div className="flex items-center">
-        <p className="mr-2">{message}</p>
+        <p>{message}</p>
       </div>
     </div>
   </div>
--- frontend/src/contexts/SnackbarProvider/SuccessSnackbar.tsx
+++ frontend/src/contexts/SnackbarProvider/SuccessSnackbar.tsx
@@ -8,7 +8,7 @@ const SuccessSnackbar = ({ message, fade, closeSnackbar }: SuccessSnackbarProps)
   <div className={`success-snackbar z-50 fixed top-0 left-1/2 transform -translate-x-1/2 mt-8 max-w-auto whitespace-nowrap ${fade}`} onClick={closeSnackbar}>
     <div className="border-t-4 rounded-b p-green px-4 py-3 shadow-md">
       <div className="flex items-center">
-        <p className="mr-2 text-white">{message}</p>
+        <p className="text-white">{message}</p>
       </div>
     </div>
   </div>
