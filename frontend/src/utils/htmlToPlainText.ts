57b39ab9fa2b9cae868b30816e9303bab6524a3b
Matěj <nkaskaj@gmail.com>
2024-02-29 19:21:23 +0100

    Added newline remover for unit issue

--- frontend/src/utils/htmlToPlainText.ts
+++ frontend/src/utils/htmlToPlainText.ts
@@ -1,4 +1,5 @@
 export const htmlToPlainText = (html: string) => {
+	// TODO: Add script tag removal to all things
 	const text = html
 		.replace(/<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi, "")
 		.replace(/<\/?(h1|h2|h3|h4|h5|h6|h7|h8|br|b|i|strong|em|a|pre|code|img|tt|div|ins|del|sup|sub|p|ol|ul|table|thead|tbody|tfoot|blockquote|dl|dt|dd|kbd|q|samp|var|hr|ruby|rt|rp|li|tr|td|th|s|strike)[^>]*>|\\n/g, " ")
@@ -7,6 +8,7 @@ export const htmlToPlainText = (html: string) => {
 		.replace(/&amp;/g, "&")
 		.replace(/&quot;/g, '"')
 		.replace(/&#39;/g, "'")
-		.replace(/&nbsp;/g, " ");
+		.replace(/&nbsp;/g, " ")
+		.replace(/\n/g, "");
 	return text;
 };
