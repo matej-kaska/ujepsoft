62ae94c31dbaa673898396570eecff184dc64652
Matěj <nkaskaj@gmail.com>
2024-05-14 19:48:18 +0200

    Updated Dockerfiles

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.http import JsonResponse
 
 urlpatterns = [
+    path("api/test/", lambda req: JsonResponse({"message": "Backend is connected!"})),
     path("admin/", admin.site.urls),
     path("api/", include("api.urls")),
     path("api/users/", include("users.urls")),
