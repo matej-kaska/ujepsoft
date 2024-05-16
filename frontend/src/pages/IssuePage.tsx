d360e576757d31481f66d379428e544517c54dca
Matěj <nkaskaj@gmail.com>
2024-05-16 12:57:00 +0200

    Fixed last line of issue/comment body is deleted

--- frontend/src/pages/IssuePage.tsx
+++ frontend/src/pages/IssuePage.tsx
@@ -51,6 +51,7 @@ const IssuePage = () => {
 			navigate("/issues");
 			return;
 		}
+		console.log(response.data);
 		if (response.data.body) response.data.body = removeFooterFromBody(response.data.body);
 		setIssue(response.data);
 		setLoading(false);
