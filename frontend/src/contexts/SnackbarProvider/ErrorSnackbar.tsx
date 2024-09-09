988d39e21c6347196d3ffd5ad620100644903f87
Matěj <nkaskaj@gmail.com>
2024-09-09 11:28:43 +0200

    Fixed snackbar z-index

--- frontend/src/contexts/SnackbarProvider/ErrorSnackbar.tsx
+++ frontend/src/contexts/SnackbarProvider/ErrorSnackbar.tsx
@@ -5,7 +5,7 @@ type ErrorSnackbarProps = {
 };
 
 const ErrorSnackbar = ({ message, fade, closeSnackbar }: ErrorSnackbarProps) => (
-	<div className={`error-snackbar z-50 fixed top-0 mt-8 max-w-md whitespace-nowrap ${fade}`} onClick={closeSnackbar}>
+	<div className={`error-snackbar fixed top-0 mt-8 max-w-md whitespace-nowrap ${fade}`} onClick={closeSnackbar}>
 		<div className="bg-red-100 border border-red-800 text-red-800 rounded relativ px-4 py-3 shadow-md">
 			<div className="flex items-center">
 				<p>{message}</p>
--- frontend/src/contexts/SnackbarProvider/SuccessSnackbar.tsx
+++ frontend/src/contexts/SnackbarProvider/SuccessSnackbar.tsx
@@ -5,7 +5,7 @@ type SuccessSnackbarProps = {
 };
 
 const SuccessSnackbar = ({ message, fade, closeSnackbar }: SuccessSnackbarProps) => (
-	<div className={`success-snackbar z-50 fixed top-0 max-w-auto whitespace-nowrap ${fade}`} onClick={closeSnackbar}>
+	<div className={`success-snackbar fixed top-0 max-w-auto whitespace-nowrap ${fade}`} onClick={closeSnackbar}>
 		<div className="border-t-4 rounded-b p-green px-4 py-3 shadow-md">
 			<div className="flex items-center">
 				<p className="text-white">{message}</p>
