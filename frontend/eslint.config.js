5837ca0e947297178f1c0e1750534801636249b1
Matěj <nkaskaj@gmail.com>
2025-02-06 14:54:48 +0100

    Fixed eslint

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,14 +1,21 @@
 import react from "eslint-plugin-react";
 import reactCompiler from "eslint-plugin-react-compiler";
+import tsPlugin from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
 
 export default [
 	{
 		plugins: {
-			react: react,
+			react,
 			"react-compiler": reactCompiler,
+			"@typescript-eslint": tsPlugin,
 		},
+		languageOptions: {
+			parser: tsParser,
+		},
+		files: ["**/*.{js,jsx,cjs,mjs,ts,tsx,cts,mts}"],
 		rules: {
-			eqeqeq: "error",
+			"eqeqeq": "error",
 			"import/no-unresolved": "off",
 			"react-hooks/exhaustive-deps": "off",
 			"jsx-a11y/click-events-have-key-events": "off",
